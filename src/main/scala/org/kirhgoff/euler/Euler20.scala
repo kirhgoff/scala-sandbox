2a955ec48dfc67183298d9f6775e6ef9c483d197
Kirill Lastovirya <kirill.lastovirya@moex.com>
2015-12-11 15:50:36 +0300

    solved euler 20

---
+++ src/main/scala/org/kirhgoff/euler/Euler20.scala
@@ -0,0 +1,22 @@
+package org.kirhgoff.euler
+
+/**
+ *
+
+n! means n × (n − 1) × ... × 3 × 2 × 1
+
+For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800,
+and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+
+Find the sum of the digits in the number 100!
+
+ * @author <a href="mailto:kirill.lastovirya@moex.com">Kirill Lastovirya</a>
+ */
+object Euler20 {
+  val factorial:BigInt = Utils.factorial(BigInt(100))
+  val digits = Palindromes.digits(factorial)
+  val sum = digits.sum
+  println("F:" + factorial)
+  println("Digits: " + digits)
+  println("Sum: " + sum)
+}
--- src/main/scala/org/kirhgoff/euler/EulerApp.scala
+++ src/main/scala/org/kirhgoff/euler/EulerApp.scala
@@ -4,6 +4,6 @@ package org.kirhgoff.euler
 object EulerApp {
 
   def main(args: Array[String]) {
-    Euler18
+    Euler20
   }
 }
