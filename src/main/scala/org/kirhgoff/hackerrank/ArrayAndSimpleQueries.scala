d7bff50dbfcfc0cdfa40fcdc355325dba7d74738
Kirill Lastovirya <kirill.lastovirya@moex.com>
2016-02-17 19:50:07 +0300

    started working on treaps

---
+++ src/main/scala/org/kirhgoff/hackerrank/ArrayAndSimpleQueries.scala
@@ -0,0 +1,46 @@
+package org.kirhgoff.hackerrank
+
+
+class Treap (val x:Int, val y:Int, var left:Treap, var right:Treap) {
+
+  def split(x:Int) : (Treap, Treap) = {
+    var resultLeft:Treap = this.left
+    var resultRight:Treap = this.right
+
+    if (this.x <= x) {
+      if (this.right == null) resultRight = null
+      else (resultLeft, resultRight) = this.right.split(x)
+
+      resultLeft = new Treap(this.x, this.y, this.left, resultLeft)
+    } else {
+      if (this.left == null) resultLeft = null
+      else (resultLeft, resultRight) = this.left.split(x)
+      resultRight = new Treap(this.x, this.y, resultLeft, this.right)
+    }
+    (resultLeft, resultRight)
+  }
+}
+
+//TODO make tail recursion
+object Treap {
+  def merge(left:Treap, right:Treap): Treap = {
+    if (left == null) return right
+    if (right == null) return left
+
+    if (left.y > right.y) new Treap(left.x, left.y, left.left, merge(left.right, right))
+    else new Treap(right.x, right.y, merge(left, right.left), right.right)
+  }
+
+}
+
+
+
+/**
+ * https://www.hackerrank.com/challenges/array-and-simple-queries
+ * @author <a href="mailto:kirill.lastovirya@moex.com">Kirill Lastovirya</a>
+ */
+object ArrayAndSimpleQueries {
+  def main(args: Array[String]) {
+
+  }
+}
